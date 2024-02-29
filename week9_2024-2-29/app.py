ab65635efbd1604e5b7834f5f49c49c33428f111
DogDog <edc.worker@gmail.com>
2024-02-29 16:29:30 +0700

    update

---
+++ week9_2024-2-29/app.py
@@ -0,0 +1,33 @@
+# app.py
+import pickle
+import warnings
+import streamlit as st
+from PIL import Image
+
+pickle_in = open('./model_iris.pkl', 'rb')
+classifier = pickle.load(pickle_in)
+
+# print('----classifier', classifier)
+
+def predic(sepal_length, sepal_width, pepal_length, pepal_width):
+  prediction = classifier.predict([[sepal_length, sepal_width, pepal_length, pepal_width]])
+  print(prediction)
+  return prediction
+
+def Input_Output():
+  st.title('my title')
+  st.image('https://imgs.gvm.com.tw/upload/gallery/20221204/125075.jpg')
+  st.markdown('you are using st', unsafe_allow_html=True)
+  sepal_length = st.text_input('enter sp length', '.')
+  sepal_width = st.text_input('enter sp width', '.')
+  pepal_length = st.text_input('enter p length', '.')
+  pepal_width = st.text_input('enter p width', '.')
+  
+  result = ''
+  if st.button('click here to Predict'):
+    result = predic(sepal_length, sepal_width, pepal_length, pepal_width)
+    st.balloons()
+
+  st.success('The output is {}'.format(result))
+
+Input_Output()
